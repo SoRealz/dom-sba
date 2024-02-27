5bf0caee81c716ee7af548d10860bd3fc8fd73f3
Jacob Sorrell <jacoblsorrell1991@gmail.com>
2024-02-27 16:45:09 -0600

    created the eventListeners

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
     const newTaskInput = document.getElementById('newTaskInput');
     const taskForm = document.getElementById('taskForm');
     const taskList = document.getElementById('taskList');
+
+       // Event listeners
+       taskForm.addEventListener('submit', addTask);
+       taskList.addEventListener('click', toggleTask);
+       newTaskInput.addEventListener('input', handleInput);
+
