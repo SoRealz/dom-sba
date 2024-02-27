133da0d2be3b8da20e885ee4bc61db5b2c145f7d
Jacob Sorrell <jacoblsorrell1991@gmail.com>
2024-02-27 16:37:23 -0600

    const added to cache elements

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-   
+    // Cache elements
+    const newTaskInput = document.getElementById('newTaskInput');
+    const taskForm = document.getElementById('taskForm');
+    const taskList = document.getElementById('taskList');
